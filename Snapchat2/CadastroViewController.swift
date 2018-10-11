860af230738a15437b59e81870cbe51b55fa3459
wesleyfpdias <projeto161718@gmail.com>
2018-10-11 16:01:52 -0300

    Cadastro de usuário

--- Snapchat2/CadastroViewController.swift
+++ Snapchat2/CadastroViewController.swift
@@ -7,9 +7,61 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class CadastroViewController: UIViewController {
 
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var senha: UITextField!
+    @IBOutlet weak var senhaConfirmacao: UITextField!
+    
+    func exibirMensagem(titulo: String, mensagem: String) {
+        
+        let alerta = UIAlertController(title: titulo, message: mensagem, preferredStyle: .alert)
+        let acaoCancelar = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        
+        alerta.addAction(acaoCancelar)
+        present(alerta, animated: true, completion: nil)
+        
+    }
+    
+    
+    @IBAction func criarConta(_ sender: Any) {
+        //Recupera dados digitados
+        if let emailR = self.email.text {
+            if let senhaR = self.senha.text {
+                if let senhaConfirmacaoR = self.senhaConfirmacao.text {
+                    
+                    //Validar senha
+                    if senhaR == senhaConfirmacaoR {
+                        
+                        //Criar conta no Firebase
+                        let autenticacao = Auth.auth()
+                        autenticacao.createUser(withEmail: emailR, password: senhaR) { (usuario, erro) in
+                            
+                            if erro == nil {
+                                print("Sucesso ao cadastrar usuário!")
+                            }else {
+                                print("Erro ao cadastrar usuário!")
+                            }
+                            
+                        }
+                        
+                    }else {
+                        
+                        self.exibirMensagem(titulo: "Dados incorretos", mensagem: "As senhas nao estao iguais, digite novamente")
+                        
+                        
+                    }//Fim validacao senha
+                    
+                }
+            }
+        }
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
