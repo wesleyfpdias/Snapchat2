e685abca97bc9bf17c7f58788d9ce3b85478e2bc
wesleyfpdias <projeto161718@gmail.com>
2018-10-11 10:40:19 -0300

    Primeiro Contato

---
+++ Snapchat2/EntrarViewController.swift
@@ -0,0 +1,41 @@
+//
+//  EntrarViewController.swift
+//  Snapchat2
+//
+//  Created by Wesley Fonseca Pivoto Dias on 05/10/18.
+//  Copyright © 2018 Wesley Fonseca Pivoto Dias. All rights reserved.
+//
+
+import UIKit
+
+class EntrarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Snapchat2/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Snapchat2
+//
+//  Created by Wesley Fonseca Pivoto Dias on 05/10/18.
+//  Copyright © 2018 Wesley Fonseca Pivoto Dias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
